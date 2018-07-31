2434d1de5e9d252d7d9bec3ac70a439ccc71aaf5
Florian Stahr <mail@florianstahr.com>
2018-07-31 22:47:08 +0200

    ADD: tooltip component

---
+++ src/components/tooltip/tooltip.component.jsx
@@ -0,0 +1,60 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import {Tooltip} from 'react-tippy';
+import FS_Defaults from '../../defaults';
+
+const position = {
+    TOP: 'top',
+    RIGHT: 'right',
+    BOTTOM: 'bottom',
+    LEFT: 'left'
+};
+
+const size = {
+    SMALL: 'small',
+    REGULAR: 'regular',
+    BIG: 'big'
+};
+
+class FS_Tooltip extends Component {
+
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        type: PropTypes.string,
+        classPrefix: PropTypes.string,
+    };
+
+    defaultTooltipProps = {
+        position: position.TOP,
+        size: size.SMALL,
+        arrow: true,
+        classPrefix: FS_Defaults.classPrefix,
+    };
+
+    render() {
+
+        const {title, children, ...other} = this.props;
+
+        const style = {
+            ...other.style,
+        };
+
+        const tooltipProps = {
+            ...this.defaultTooltipProps,
+            ...other,
+            style,
+            title,
+        };
+
+        return <Tooltip
+            {...tooltipProps}
+        >
+            {children}
+        </Tooltip>
+    }
+}
+
+FS_Tooltip.size = size;
+FS_Tooltip.position = position;
+
+export default FS_Tooltip;
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// tooltip
+import FS_Tooltip from './components/tooltip/tooltip.component';
+
+const FS_Components = {
+    FS_Tooltip
+};
+
+export default FS_Components;
