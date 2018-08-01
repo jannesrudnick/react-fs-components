39097fce928ad4fc8c4058284d9aee8432ffd4eb
Florian Stahr <mail@florianstahr.com>
2018-08-01 15:04:38 +0200

    ADD: action-btns component

---
+++ src/components/action-btns/action-btns-group.component.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import map from 'lodash/map';
+import classNames from 'classnames';
+import FS_Defaults from '../../defaults';
+import FS_ActionBtn from './action-btn.component';
+
+class FS_ActionBtnsGroup extends Component {
+
+    static propTypes = {
+        btns: PropTypes.arrayOf(
+            PropTypes.shape({
+                containerClassNames: PropTypes.array,
+                icon: PropTypes.element,
+                iconAfter: PropTypes.bool,
+                text: PropTypes.any,
+                onClick: PropTypes.func,
+                isElement: PropTypes.bool,
+                element: PropTypes.element,
+                highlight: PropTypes.bool,
+            })
+        ).isRequired,
+        classPrefix: PropTypes.string,
+    };
+
+    static defaultProps = {
+        classPrefix: FS_Defaults.classPrefix
+    };
+
+    render() {
+
+        const {btns, classPrefix} = this.props;
+
+        return <div className={classNames(
+            classPrefix + 'action-btn-group'
+        )}>
+            {map(btns, (btn, index) => {
+                return <FS_ActionBtn
+                    key={classPrefix + ':action-btns:group:btn:' + index} {...btn}
+                    isGroup={true}
+                />
+            })}
+        </div>
+    }
+}
+
+export default FS_ActionBtnsGroup;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,18 @@
 
+// action btns
+import FS_ActionBtns from './components/action-btns/action-btns.component';
+import FS_ActionBtnsGroup from './components/action-btns/action-btns-group.component';
+import FS_ActionBtn from './components/action-btns/action-btn.component';
+
 // tooltip
 import FS_Tooltip from './components/tooltip/tooltip.component';
 
 const FS_Components = {
-    FS_Tooltip
+    FS_ActionBtns,
+    FS_ActionBtnsGroup,
+    FS_ActionBtn,
+
+    FS_Tooltip,
 };
 
 export default FS_Components;
