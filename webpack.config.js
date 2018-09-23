82a84e0612d10ed529e1d1a72ea83b192e0288f4
Florian Stahr <mail@florianstahr.com>
2018-09-23 15:45:15 +0200

    ADD: webpack

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const path = require('path');
+
+const libraryName = 'reactFSComponents';
+const outputFile = 'react-fs-components.js';
+const plugins = [];
+
+module.exports = {
+  entry: path.join(__dirname, "./src/index.js"),
+  output: {
+    path: path.join(__dirname, './dist'),
+    filename: outputFile,
+    library: libraryName,
+    libraryTarget: 'umd',
+    publicPath: '/dist/',
+    umdNamedDefine: true
+  },
+  module: {
+    rules : [
+      {
+        test: /\.(js|jsx)$/,
+        use: {
+          loader: 'babel-loader',
+        },
+        include: path.resolve(__dirname, "src"),
+        exclude: /node_modules/,
+      },
+    ]
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      'react': path.resolve(__dirname, './node_modules/react') ,
+      'react-dom': path.resolve(__dirname, './node_modules/react-dom')
+    }
+  },
+  externals: {
+    // Don't bundle react or react-dom
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React"
+    },
+    'react-dom': {
+      root: 'ReactDOM',
+      commonjs2: 'react-dom',
+      commonjs: 'react-dom',
+      amd: 'react-dom',
+      umd: 'react-dom',
+    },
+  }
+};
