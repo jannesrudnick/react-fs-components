a4b1ee20309470b5cec21d550ef512c551dc8ba4
Florian Stahr <mail@florianstahr.com>
2018-10-16 04:23:43 +0200

    ADD: component FS_Toggle

---
+++ src/components/toggle/toggle.component.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Toggle from 'react-toggle';
+import classnames from 'classnames';
+import FS_Defaults from '../../defaults';
+
+console.log('uuu');
+
+class FS_Toggle extends Component {
+  static propTypes = {
+    title: PropTypes.string.isRequired,
+    checked: PropTypes.bool.isRequired,
+    onChange: PropTypes.func.isRequired,
+    containerClassNames: PropTypes.array,
+    classPrefix: PropTypes.string,
+    disabled: PropTypes.bool,
+  };
+
+  static defaultProps = {
+    classPrefix: FS_Defaults.classPrefix,
+    disabled: false,
+    containerClassNames: null,
+  };
+
+  handleChange = (e) => {
+    const { onChange } = this.props;
+    onChange(e.target.checked);
+  };
+
+  render() {
+    const {
+      title, checked, containerClassNames, classPrefix, disabled,
+    } = this.props;
+
+    return (
+      <div
+        className={classnames(
+          `${classPrefix}toggle`,
+          containerClassNames,
+        )}
+      >
+        <div className={`${classPrefix}toggle-label`}>
+          <Toggle
+            checked={checked}
+            onChange={this.handleChange}
+            disabled={disabled}
+          />
+          <span className={`${classPrefix}toggle-label-text`}>
+            {title}
+          </span>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default FS_Toggle;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import FS_ActionBtns from './components/action-btns/action-btns.component';
 import FS_ActionBtnsGroup from './components/action-btns/action-btns-group.component';
 import FS_ActionBtn from './components/action-btns/action-btn.component';
 
+// toggle
+import FS_Toggle from './components/toggle/toggle.component';
+
 // tooltip
 import FS_Tooltip from './components/tooltip/tooltip.component';
 
@@ -15,6 +18,8 @@ export {
   FS_ActionBtns,
   FS_ActionBtnsGroup,
   FS_ActionBtn,
+
+  FS_Toggle,
 
   FS_Tooltip,
 };
