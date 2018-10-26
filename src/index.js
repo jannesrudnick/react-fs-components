02cbc8d810baadedd544ec04f50b3a2ac3ee23d5
Florian Stahr <mail@florianstahr.com>
2018-10-26 17:39:06 +0200

    ADD: component FS_Select

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ import FS_ActionBtns from './components/action-btns/action-btns.component';
 import FS_ActionBtnsGroup from './components/action-btns/action-btns-group.component';
 import FS_ActionBtn from './components/action-btns/action-btn.component';
 
+// select
+import FS_Select from './components/select/select';
+
 // split
 import FS_Split from './components/split/split';
 
@@ -24,6 +27,8 @@ export {
   FS_ActionBtns,
   FS_ActionBtnsGroup,
   FS_ActionBtn,
+
+  FS_Select,
 
   FS_Split,
 
